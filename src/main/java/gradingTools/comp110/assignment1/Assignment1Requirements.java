f039126270ce58aaf0f0a8ccb9c8622f5d1f3f85
Jacob Bartel <bartel@cs.unc.edu>
2014-01-31 15:49:16 -0500

    Moved manual case to the bottom for easier visibility in the UI

--- src/main/java/gradingTools/comp110/assignment1/Assignment1Requirements.java
+++ src/main/java/gradingTools/comp110/assignment1/Assignment1Requirements.java
@@ -26,15 +26,14 @@ public class Assignment1Requirements extends FrameworkProjectRequirements {
 		// Check for adding and multiplying of numbers
 		addFeature("Adds and multiplies the numbers", 10, new AddAndMultiplyTestCase());
 
-		// Check for the messages with the outputs
-		addFeature("Messages with output", 10, new ManualTestCase(
-				"Prints clear messages for each of the outputs"));
-
 		// Check for the results of addition and multiplication
 		addFeature("Displays results of addition and multiplication", 40,
 				new PrintAddAndMultiplyTestCase());
 
 		addFeature("Has a proper header", 10, new ProperHeaderTestCase());
 
+		// Check for the messages with the outputs
+		addFeature("Messages with output", 10, new ManualTestCase(
+				"Prints clear messages for each of the outputs"));
 	}
 }
