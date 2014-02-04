06fbfb153284ad43dab7847964c3ff80c2978baa
Jacob Bartel <bartel@cs.unc.edu>
2014-02-03 23:03:44 -0500

    Fixed issue with HelloWorld class being in separate package

--- src/main/java/gradingTools/comp110/program0/HelloWorldClassTestCase.java
+++ src/main/java/gradingTools/comp110/program0/HelloWorldClassTestCase.java
@@ -10,27 +10,31 @@ import framework.project.Project;
 
 public class HelloWorldClassTestCase extends BasicTestCase {
 
-    public HelloWorldClassTestCase() {
-        super("HelloWorld class test case");
-    }
+	public HelloWorldClassTestCase() {
+		super("HelloWorld class test case");
+	}
 
-    @Override
-    public TestCaseResult test(Project project, boolean autoGrade) throws NotAutomatableException, NotGradableException {
+	@Override
+	public TestCaseResult test(Project project, boolean autoGrade) throws NotAutomatableException,
+			NotGradableException {
 
-        // You can get the classes and information about them from the project using the ClassesManager
-        if (project.getClassesManager().isEmpty())
-            throw new NotGradableException();
-        ClassesManager manager = project.getClassesManager().get();
+		// You can get the classes and information about them from the project
+		// using the ClassesManager
+		if (project.getClassesManager().isEmpty())
+			throw new NotGradableException();
+		ClassesManager manager = project.getClassesManager().get();
 
-        // Check for a class with the name HelloWorld
-        for (ClassDescription description : manager.getClassDescriptions()) {
-        	if (description.getJavaClass().getName().equals("HelloWorld")) {
-        		return pass();
-        	} else if (description.getJavaClass().getName().equalsIgnoreCase("HelloWorld")) {
-        		return partialPass(0.5, "HelloWorld class name does not contain proper capitalization");
-        	}
-        }
+		// Check for a class with the name HelloWorld
+		for (ClassDescription description : manager.getClassDescriptions()) {
+			if (description.getJavaClass().getName().equals("HelloWorld")
+					|| description.getJavaClass().getName().matches(".+[.]HelloWorld")) {
+				return pass();
+			} else if (description.getJavaClass().getName().equalsIgnoreCase("HelloWorld")) {
+				return partialPass(0.5,
+						"HelloWorld class name does not contain proper capitalization");
+			}
+		}
 
-        return fail("No class with name HelloWorld found");
-    }
+		return fail("No class with name HelloWorld found");
+	}
 }
--- src/main/java/gradingTools/comp110/program0/MainMethodTestCase.java
+++ src/main/java/gradingTools/comp110/program0/MainMethodTestCase.java
@@ -13,67 +13,72 @@ import framework.project.Project;
 
 public class MainMethodTestCase extends BasicTestCase {
 
-    public MainMethodTestCase() {
-        super("HelloWorld class test case");
-    }
+	public MainMethodTestCase() {
+		super("HelloWorld class test case");
+	}
 
-    @Override
-    public TestCaseResult test(Project project, boolean autoGrade) throws NotAutomatableException, NotGradableException {
+	@Override
+	public TestCaseResult test(Project project, boolean autoGrade) throws NotAutomatableException,
+			NotGradableException {
 
-        // You can get the classes and information about them from the project using the ClassesManager
-        if (project.getClassesManager().isEmpty())
-            throw new NotGradableException();
-        ClassesManager manager = project.getClassesManager().get();
+		// You can get the classes and information about them from the project
+		// using the ClassesManager
+		if (project.getClassesManager().isEmpty())
+			throw new NotGradableException();
+		ClassesManager manager = project.getClassesManager().get();
 
-        // Find the class class with the name HelloWorld
-        for (ClassDescription description : manager.getClassDescriptions()) {
-        	Class javaClass = description.getJavaClass();
-        	if (javaClass.getName().equalsIgnoreCase("HelloWorld")) {
-        		for (Method method : javaClass.getDeclaredMethods()) {
-        			
-        			// Check that the signature is public static void main (String[] ...)
-        			
-        			boolean correctName = method.getName().equals("main");
-        			boolean correctVisibility = Modifier.isPublic(method.getModifiers());
-        			boolean correctStatic = Modifier.isStatic(method.getModifiers());
-        			boolean correctReturnType = method.getReturnType().equals(Void.TYPE);
-        			boolean correctParameterTypes = false;
-        			Class[] paramClasses = method.getParameterTypes();
-        			if (paramClasses.length == 1) {
-        				if (paramClasses[0].equals((new String[0]).getClass())) {
-        					correctParameterTypes = true;
-        				}
-        			}
-        			
-        			if (correctName && correctVisibility && correctStatic && correctReturnType && correctParameterTypes) {
-        				return pass();
-        			} else if (correctName) {
-        				int incorrectCount = 0;
-        				String message = "";
-        				if (!correctVisibility) {
-        					incorrectCount++;
-        					message += "main method not public, ";
-        				}
-        				if (!correctStatic) {
-        					incorrectCount++;
-        					message += "main method not static, ";
-        				}
-        				if (!correctReturnType) {
-        					incorrectCount++;
-        					message += "main method does not return void, ";
-        				}
-        				if (!correctParameterTypes) {
-        					incorrectCount++;
-        					message += "main does not take String[] as its only parameter, ";
-        				}
-        				
-        				message = message.substring(0,message.length()-2);
-        				return partialPass((5.0-incorrectCount)/5.0, message);
-        			}
-        		}
-        	}
-        }
+		// Find the class class with the name HelloWorld
+		for (ClassDescription description : manager.getClassDescriptions()) {
+			Class javaClass = description.getJavaClass();
+			if (javaClass.getName().equalsIgnoreCase("HelloWorld")
+					|| description.getJavaClass().getName().matches(".+[.]HelloWorld")) {
+				for (Method method : javaClass.getDeclaredMethods()) {
 
-        return fail("No method main found in class with name HelloWorld");
-    }
+					// Check that the signature is public static void main
+					// (String[] ...)
+
+					boolean correctName = method.getName().equals("main");
+					boolean correctVisibility = Modifier.isPublic(method.getModifiers());
+					boolean correctStatic = Modifier.isStatic(method.getModifiers());
+					boolean correctReturnType = method.getReturnType().equals(Void.TYPE);
+					boolean correctParameterTypes = false;
+					Class[] paramClasses = method.getParameterTypes();
+					if (paramClasses.length == 1) {
+						if (paramClasses[0].equals((new String[0]).getClass())) {
+							correctParameterTypes = true;
+						}
+					}
+
+					if (correctName && correctVisibility && correctStatic && correctReturnType
+							&& correctParameterTypes) {
+						return pass();
+					} else if (correctName) {
+						int incorrectCount = 0;
+						String message = "";
+						if (!correctVisibility) {
+							incorrectCount++;
+							message += "main method not public, ";
+						}
+						if (!correctStatic) {
+							incorrectCount++;
+							message += "main method not static, ";
+						}
+						if (!correctReturnType) {
+							incorrectCount++;
+							message += "main method does not return void, ";
+						}
+						if (!correctParameterTypes) {
+							incorrectCount++;
+							message += "main does not take String[] as its only parameter, ";
+						}
+
+						message = message.substring(0, message.length() - 2);
+						return partialPass((5.0 - incorrectCount) / 5.0, message);
+					}
+				}
+			}
+		}
+
+		return fail("No method main found in class with name HelloWorld");
+	}
 }
