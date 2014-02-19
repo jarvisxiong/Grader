7ddd021fa8769342e864bb2bad7a37cc3b64daa9
Jacob Bartel <bartel@cs.unc.edu>
2014-02-18 20:49:30 -0500

    Reduce number of times it needs to run and checks for additional incorrect output

--- src/main/java/gradingTools/comp110/assignment2/testcases/ErrorTestCase.java
+++ src/main/java/gradingTools/comp110/assignment2/testcases/ErrorTestCase.java
@@ -2,42 +2,32 @@ package gradingTools.comp110.assignment2.testcases;
 
 import framework.execution.NotRunnableException;
 import framework.execution.RunningProject;
-import framework.grading.testing.BasicTestCase;
 import framework.grading.testing.NotAutomatableException;
 import framework.grading.testing.NotGradableException;
 import framework.grading.testing.TestCaseResult;
 import framework.project.Project;
 import gradingTools.utils.RunningProjectUtils;
 
-public class ErrorTestCase extends BasicTestCase {
+public class ErrorTestCase extends PalindromeTestCase {
 
 	public ErrorTestCase() {
 		super("Error Message Test Case");
 	}
 
-	private boolean hasErrorMessage(Project project, String input) throws NotGradableException {
+	private boolean hasErrorMessage(Project project, String input) throws NotGradableException,
+			NotAutomatableException {
 
 		try {
-			RunningProject runningProject = RunningProjectUtils.runProject(project, 3);
-			String output = runningProject.await();
-			if (output.endsWith("\n")) {
-				output = output.substring(0, output.length() - 1);
+			RunningProject runningProject = RunningProjectUtils.runProject(project, 1);
+			String prompt = runningProject.await();
+			if (prompt.endsWith("\n")) {
+				prompt = prompt.substring(0, prompt.length() - 1);
 			}
-			RunningProject runningProjectWithInput = RunningProjectUtils.runProject(project, 3,
-					input);
-			String outputAfterInput = runningProjectWithInput.await();
 
-			// Now you can test that the output has an error message
-			if (outputAfterInput.length() > output.length() && outputAfterInput.startsWith(output)) {
-				String errorMessage = outputAfterInput.substring(output.length());
-				if (errorMessage.toLowerCase().contains("error")) {
-					return true;
-				} else {
-					return false;
-				}
-			} else {
-				return false;
-			}
+			String[] isError = { "Error" };
+			String[] isNotError = { "is not a palindrome", "is a palindrome" };
+
+			return resultOfInputMatches(project, prompt, input, isError, isNotError);
 
 		} catch (NotRunnableException e) {
 			throw new NotGradableException();
--- src/main/java/gradingTools/comp110/assignment2/testcases/PalindromeIgnoreCaseTestCase.java
+++ src/main/java/gradingTools/comp110/assignment2/testcases/PalindromeIgnoreCaseTestCase.java
@@ -1,12 +1,18 @@
 package gradingTools.comp110.assignment2.testcases;
 
 import framework.execution.NotRunnableException;
+import framework.execution.RunningProject;
 import framework.grading.testing.NotAutomatableException;
 import framework.grading.testing.NotGradableException;
 import framework.grading.testing.TestCaseResult;
 import framework.project.Project;
+import gradingTools.utils.RunningProjectUtils;
 
 public class PalindromeIgnoreCaseTestCase extends PalindromeTestCase {
+
+	public PalindromeIgnoreCaseTestCase() {
+		super("Palindrome ignoring capitalization test case");
+	}
 
 	@Override
 	public TestCaseResult test(Project project, boolean autoGrade) throws NotAutomatableException,
@@ -16,7 +22,17 @@ public class PalindromeIgnoreCaseTestCase extends PalindromeTestCase {
 
 			String casedPalindrome = "A!bB!a";
 
-			if (resultOfInputContains(project, casedPalindrome, "is a palindrome")) {
+			RunningProject runningProject = RunningProjectUtils.runProject(project, 1);
+			String prompt = runningProject.await();
+			if (prompt.endsWith("\n")) {
+				prompt = prompt.substring(0, prompt.length() - 1);
+			}
+
+			String[] isPalindrome = { "is a palindrome" };
+			String[] isNotPalindrome = { "is not a palindrome", "Error" };
+
+			if (resultOfInputMatches(project, prompt, casedPalindrome, isPalindrome,
+					isNotPalindrome)) {
 				return pass();
 			} else {
 				return fail("Does not ignore case when detecting palindromes");
