424c625611c945acb98336361a898d7ee0da79d9
pdewan <dewan@cs.unc.edu>
2014-02-26 09:45:26 -0500

    Tried to change color of grading features with current oe. Looks like oe will have to be changed

--- src/main/java/grader/sakai/project/AnOEProjectStepperDisplayer.java
+++ src/main/java/grader/sakai/project/AnOEProjectStepperDisplayer.java
@@ -1,11 +1,23 @@
 package grader.sakai.project;
 
+import java.awt.Color;
+
+import grader.assignment.GradingFeatureList;
 import bus.uigen.OEFrame;
 import bus.uigen.ObjectEditor;
+import bus.uigen.uiFrame;
+import bus.uigen.attributes.AttributeNames;
+import bus.uigen.oadapters.ObjectAdapter;
 
 public class AnOEProjectStepperDisplayer implements ProjectStepperDisplayer<OEFrame> {
 	public OEFrame display(ProjectStepper aProjectStepper) {
-		OEFrame oeFrame = ObjectEditor.edit(aProjectStepper);
+		uiFrame oeFrame = ObjectEditor.edit(aProjectStepper);
+		GradingFeatureList gradingFeatures = aProjectStepper.getGradingFeatures();
+		ObjectAdapter objectAdapter = oeFrame.getObjectAdapter(gradingFeatures.get(0));
+		objectAdapter.setLocalAttribute(AttributeNames.COMPONENT_BACKGROUND, Color.PINK);
+		objectAdapter.setTempAttributeValue(AttributeNames.COMPONENT_BACKGROUND, Color.PINK);
+
+		oeFrame.refresh();
 //		oeFrame.setLocation(700, 500);
 		oeFrame.setLocation(0, 0);
 		oeFrame.setSize(650, 800);
