726b0ef71383d563c5f440367a00fd28138da2fc
pdewan <dewan@cs.unc.edu>
2014-03-04 10:58:10 -0500

    Finished mimicking Josh's UI using OE. Need to attach it to the persistent data next

--- src/main/java/grader/start/AnOnyenRangeModelAR.java
+++ src/main/java/grader/start/AnOnyenRangeModelAR.java
@@ -7,7 +7,7 @@ import bus.uigen.undo.ExecutableCommand;
 public class AnOnyenRangeModelAR implements ExecutableCommand{
 	public Object execute(Object theFrame) {
 		ObjectEditor.setPropertyAttribute(AnOnyenRangeModel.class, "startingOnyen",  AttributeNames.STRETCHABLE_BY_PARENT, true);
-
+		ObjectEditor.setPropertyAttribute(AnOnyenRangeModel.class, "endingOnyen",  AttributeNames.STRETCHABLE_BY_PARENT, true);
 		ObjectEditor.setAttribute(AnOnyenRangeModel.class,  AttributeNames.LAYOUT, AttributeNames.GRID_BAG_LAYOUT);
 		ObjectEditor.setAttribute(AnOnyenRangeModel.class,  AttributeNames.STRETCHABLE_BY_PARENT, true);
 
