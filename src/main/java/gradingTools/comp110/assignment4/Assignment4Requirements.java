6174ee2d133bc43be301dc43264e25cccf132930
Maxwell Daum <daumisgod@gmail.com>
2014-04-14 11:14:32 -0400

    Have new main method test case that will make sure I am looking in right class. Also this is the state I left the grader in when we ran into the bug. Apologies for neglecting to push this it slipped my mn

--- src/main/java/gradingTools/comp110/assignment4/Assignment4Requirements.java
+++ src/main/java/gradingTools/comp110/assignment4/Assignment4Requirements.java
@@ -3,23 +3,23 @@ package gradingTools.comp110.assignment4;
 import java.util.ArrayList;
 
 import framework.grading.FrameworkProjectRequirements;
-import gradingTools.comp110.assignment1.testcases.MainMethodTestCase;
 import gradingTools.comp110.assignment1.testcases.ProperHeaderTestCase;
+import gradingTools.comp110.assignment4.testCases.HasMainMethodMultClasses;
 import gradingTools.sharedTestCase.HasMethodTestCase;
 
 public class Assignment4Requirements extends FrameworkProjectRequirements {
 	public Assignment4Requirements() {
-		addDueDate("04/11/2014 23:55:59", 1.0);
-		addDueDate("04/12/2014 23:55:59", 0.5);
+		addDueDate("04/16/2014 23:55:59", 1.0);
+		addDueDate("04/17/2014 23:55:59", 0.5);
 
 		// Checks for proper header
-		addFeature("Has a proper header", 5, new ProperHeaderTestCase());
+		//addFeature("Has a proper header", 5, new ProperHeaderTestCase());
 
 		// Check for a main method
-		addFeature("Contains a main method", 5, new MainMethodTestCase());
+		addFeature("Contains a main method", 5, new HasMainMethodMultClasses());
 
 		// Classes not to check for our methods
-		ArrayList<String> badClasses = new ArrayList<String>();
+		/*ArrayList<String> badClasses = new ArrayList<String>();
 		badClasses.add("gerbil");
 		badClasses.add("food");
 
@@ -31,6 +31,6 @@ public class Assignment4Requirements extends FrameworkProjectRequirements {
 		Class<?>[] searchForGerbilParameterTypes = { String.class };
 		addFeature("Has proper searchForGerbil method", 15, new HasMethodTestCase(
 				"searchForGerbil", badClasses, String.class, searchForGerbilParameterTypes));
-	}
+	*/}
 
 }
