65db1e70152e08b3d3a42d00dada892f5bcfc6c4
Jacob Bartel <bartel@cs.unc.edu>
2014-04-15 15:12:21 -0400

    Added simple working test for Assignment4

--- src/main/java/gradingTools/comp110/assignment4/Assignment4Requirements.java
+++ src/main/java/gradingTools/comp110/assignment4/Assignment4Requirements.java
@@ -1,11 +1,7 @@
 package gradingTools.comp110.assignment4;
 
-import java.util.ArrayList;
-
 import framework.grading.FrameworkProjectRequirements;
 import gradingTools.comp110.assignment1.testcases.ProperHeaderTestCase;
-import gradingTools.comp110.assignment4.testCases.HasMainMethodMultClasses;
-import gradingTools.sharedTestCase.HasMethodTestCase;
 
 public class Assignment4Requirements extends FrameworkProjectRequirements {
 	public Assignment4Requirements() {
@@ -13,24 +9,26 @@ public class Assignment4Requirements extends FrameworkProjectRequirements {
 		addDueDate("04/17/2014 23:55:59", 0.5);
 
 		// Checks for proper header
-		//addFeature("Has a proper header", 5, new ProperHeaderTestCase());
+		addFeature("Has a proper header", 5, new ProperHeaderTestCase());
 
 		// Check for a main method
-		addFeature("Contains a main method", 5, new HasMainMethodMultClasses());
+		// addFeature("Contains a main method", 5, new
+		// HasMainMethodMultClasses());
 
 		// Classes not to check for our methods
-		/*ArrayList<String> badClasses = new ArrayList<String>();
-		badClasses.add("gerbil");
-		badClasses.add("food");
-
-		// Check for averageFood method
-		addFeature("Has proper averageFood method", 15, new HasMethodTestCase("averageFood",
-				badClasses, String.class));
-
-		// Check for searchForGerbil method
-		Class<?>[] searchForGerbilParameterTypes = { String.class };
-		addFeature("Has proper searchForGerbil method", 15, new HasMethodTestCase(
-				"searchForGerbil", badClasses, String.class, searchForGerbilParameterTypes));
-	*/}
+		/*
+		 * ArrayList<String> badClasses = new ArrayList<String>();
+		 * badClasses.add("gerbil"); badClasses.add("food");
+		 * 
+		 * // Check for averageFood method
+		 * addFeature("Has proper averageFood method", 15, new
+		 * HasMethodTestCase("averageFood", badClasses, String.class));
+		 * 
+		 * // Check for searchForGerbil method Class<?>[]
+		 * searchForGerbilParameterTypes = { String.class };
+		 * addFeature("Has proper searchForGerbil method", 15, new
+		 * HasMethodTestCase( "searchForGerbil", badClasses, String.class,
+		 * searchForGerbilParameterTypes));
+		 */}
 
 }
