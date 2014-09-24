cf995458cc2b254095b0e5c822d933ff2d7cd6d0
Maxwell Daum <daumisgod@gmail.com>
2014-09-24 19:58:36 -0400

    updated due date for Lab2

--- src/main/java/gradingTools/comp110f14/Lab2/Lab2Requirements.java
+++ src/main/java/gradingTools/comp110f14/Lab2/Lab2Requirements.java
@@ -6,8 +6,8 @@ import gradingTools.comp110f14.Lab2.testcases.RestTestCase;
 
 public class Lab2Requirements extends FrameworkProjectRequirements {//Labs out of 10
 	public Lab2Requirements(){
-		addDueDate("09/16/2014 23:55:59", 1.0);//temp
-		addDueDate("09/17/2014 23:55:59", 0.5);//temp
+		addDueDate("09/22/2014 23:55:59", 1.0);//temp
+		addDueDate("09/23/2014 23:55:59", 0.5);//temp
 		
 		//check for main method
 		addFeature("Contains a main method", 5, new MainMethodTestCase());
