0f47b87f7db846c814bda996b98072c5620777eb
Jacob Bartel <bartel@cs.unc.edu>
2014-10-23 12:19:13 -0400

    Added style check for capitalization of names

---
+++ src/main/java/gradingTools/comp110f14/assignment4testcases/CapitalizationStyle.java
@@ -0,0 +1,72 @@
+package gradingTools.comp110f14.assignment4testcases;
+
+import framework.grading.testing.NotAutomatableException;
+import framework.grading.testing.NotGradableException;
+import framework.grading.testing.TestCaseResult;
+import framework.project.Project;
+import gradingTools.sharedTestCase.CodeInspectorTestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CapitalizationStyle extends CodeInspectorTestCase {
+	
+	List<String> incorrectCases;
+	
+	
+	public CapitalizationStyle() {
+		super("Correct capitalization of variables, methods, and classes");
+	}
+	
+	@Override
+	public TestCaseResult test(Project project, boolean autoGrade) throws NotAutomatableException,
+	NotGradableException {
+		incorrectCases = new ArrayList<>();
+		return super.test(project, autoGrade);
+	}
+	
+	private boolean isUpperCase(char c) {
+		return c >= 'A' && c <= 'Z';
+	}
+	
+	private boolean isLowerCase(char c) {
+		return c >= 'a' && c <= 'z';
+	}
+
+	@Override
+	public void inspectClassOrInterfaceName(String name) {
+		if (name.length() > 0 && isLowerCase(name.charAt(0))) {
+			incorrectCases.add(name);
+		}
+	}
+
+	@Override
+	public void inspectMethodName(String name) {
+		if (name.length() > 0 && isUpperCase(name.charAt(0))) {
+			incorrectCases.add(name);
+		}
+	}
+
+	@Override
+	public void inspectVariableName(String variable) {
+		if (name.length() > 0 && isUpperCase(name.charAt(0))) {
+			incorrectCases.add(name);
+		}
+	}
+
+	@Override
+	public TestCaseResult codeInspectionResult() {
+		if (incorrectCases.size() > 0) {
+			return pass();
+		} else {
+			String wrongNames = "";
+			for (int i=0; i < incorrectCases.size(); i++) {
+				if (i>0){
+					wrongNames += ", ";
+				}
+				wrongNames += incorrectCases.get(i);
+			}
+			return fail("The following names start with the incorrect case: "+wrongNames);
+		}
+	}
+}
