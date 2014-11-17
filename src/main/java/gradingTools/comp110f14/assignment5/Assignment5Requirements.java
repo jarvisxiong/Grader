533d26526366e225264826be9c87940d905a66f2
Maxwell Daum <daumisgod@gmail.com>
2014-11-17 10:10:43 -0500

    started A5 grader

---
+++ src/main/java/gradingTools/comp110f14/assignment5/Assignment5Requirements.java
@@ -0,0 +1,16 @@
+package gradingTools.comp110f14.assignment5;
+
+import framework.grading.FrameworkProjectRequirements;
+
+
+public class Assignment5Requirements extends FrameworkProjectRequirements {
+	public Assignment5Requirements(){
+		addDueDate("10/31/2014 23:55:59", 1.0);//temp
+		addDueDate("11/01/2014 23:55:59", 0.5);//temp
+		
+
+		// Check for Header
+		
+		
+	}
+}
