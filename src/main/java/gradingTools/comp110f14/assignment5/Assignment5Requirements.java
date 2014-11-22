1df4f647621a5cd9c699bad93fb03c0e73f90206
unknown <mdaum@compsci914.cs.unc.edu>
2014-11-22 17:20:48 -0500

    Started A5 Grader

--- src/main/java/gradingTools/comp110f14/assignment5/Assignment5Requirements.java
+++ src/main/java/gradingTools/comp110f14/assignment5/Assignment5Requirements.java
@@ -1,16 +1,20 @@
 package gradingTools.comp110f14.assignment5;
 
 import framework.grading.FrameworkProjectRequirements;
+import gradingTools.comp110.assignment1.testcases.ProperHeaderTestCase;
+import gradingTools.comp110f14.assignment4testcases.CapitalizationStyle;
 
 
 public class Assignment5Requirements extends FrameworkProjectRequirements {
 	public Assignment5Requirements(){
-		addDueDate("10/31/2014 23:55:59", 1.0);//temp
-		addDueDate("11/01/2014 23:55:59", 0.5);//temp
+		addDueDate("11/21/2014 23:55:59", 1.0);
+		addDueDate("11/22/2014 23:55:59", 0.5);
 		
-
 		// Check for Header
-		
+		addFeature("Has a proper header", 5, new ProperHeaderTestCase("COMP110-001, Fall 2014"));
+		//Check for Capitalization
+		addFeature("Naming Style",5,new CapitalizationStyle());
+		//Initial Prompt
 		
 	}
 }
