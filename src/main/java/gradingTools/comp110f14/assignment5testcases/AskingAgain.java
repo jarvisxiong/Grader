8ee66bfd51dbe990732bc6d7452b44af59404722
unknown <sgbyers@live.unc.edu>
2014-11-22 19:00:12 -0500

    added stuff

---
+++ src/main/java/gradingTools/comp110f14/assignment5testcases/AskingAgain.java
@@ -0,0 +1,41 @@
+package gradingTools.comp110f14.assignment5testcases;
+
+import java.util.regex.Pattern;
+
+import framework.execution.RunningProject;
+import framework.grading.testing.BasicTestCase;
+import framework.grading.testing.NotAutomatableException;
+import framework.grading.testing.NotGradableException;
+import framework.grading.testing.TestCaseResult;
+import framework.project.Project;
+import gradingTools.utils.RunningProjectUtils;
+
+public class AskingAgain extends BasicTestCase{
+
+	public AskingAgain() {
+		//mandatory super call to create instance of BasicTestCase w/ message
+		super("Asking Again Test Case");
+	}
+
+	@Override
+	public TestCaseResult test(Project project, boolean autoGrade)
+			throws NotAutomatableException, NotGradableException {
+		//pattern searching for
+		Pattern again = Pattern.compile(".*another|message.*");
+		//invoke instance of assignment five input string w/ timeout
+		RunningProject againInfo = RunningProjectUtils.runProject(project, 10, "ToBeOrNotToBe");
+		//grab everything after input up to point waiting for input
+		String outagainInfo= againInfo.await();
+
+		//If dont ask for another message - fail w/ message
+		if(!again.matcher(outagainInfo).find()){
+			return fail("Failed to ask again");
+		}
+		
+		//asks for another message
+		return pass();
+		
+	}
+
+	
+}
--- src/main/java/gradingTools/comp110f14/assignment5testcases/PromptTestCase.java
+++ src/main/java/gradingTools/comp110f14/assignment5testcases/PromptTestCase.java
@@ -1,5 +1,35 @@
 package gradingTools.comp110f14.assignment5testcases;
 
-public class PromptTestCase {
+import framework.execution.RunningProject;
+import framework.grading.testing.BasicTestCase;
+import framework.grading.testing.NotAutomatableException;
+import framework.grading.testing.NotGradableException;
+import framework.grading.testing.TestCaseResult;
+import framework.project.Project;
+import gradingTools.utils.RunningProjectUtils;
 
+//signature includses extends BasicTestCase in order to implement test
+public class PromptTestCase extends BasicTestCase {
+	//mandatory constructor to invoke test case
+	public PromptTestCase() {
+		//Call to super BasicTestCase to create instance
+		super("Prompt Test Case");
+		// TODO Auto-generated constructor stub
+	}
+	
+	//actual test case with necessary exceptions
+	public TestCaseResult test(Project project, boolean autoGrade)
+			throws NotAutomatableException, NotGradableException {
+		//invoke instance of code and time out
+		RunningProject Project0 = RunningProjectUtils.runProject(project, 10,"");
+		//grabs user prompt String and sets it to lower case so ignores case
+		String prompt=Project0.await().toLowerCase();
+		//initialize boolean
+		boolean hasPrompt=false;
+		//search for desired string
+		if(prompt.contains("enter"))hasPrompt=true;
+		//return pass or fail with prompt
+		if(hasPrompt)return pass();
+		return fail("Did not correctly prompt for user input");
+	}
 }
