a6475fc767fb6092986da785e3dba5e9203121a6
Maxwell Daum <daumisgod@gmail.com>
2014-12-04 13:31:06 -0500

    started A6 tester

---
+++ src/main/java/gradingTools/comp110f14/assignment6/Assignment6Requirements.java
@@ -0,0 +1,19 @@
+package gradingTools.comp110f14.assignment6;
+import framework.grading.FrameworkProjectRequirements;
+import gradingTools.comp110.assignment1.testcases.ProperHeaderTestCase;
+import gradingTools.comp110f14.assignment4testcases.CapitalizationStyle;
+
+public class Assignment6Requirements extends FrameworkProjectRequirements {
+	
+	public Assignment6Requirements(){
+		addDueDate("12/03/2014 23:55:59", 1.0);
+		addDueDate("12/04/2014 23:55:59", 0.5);
+		
+		// Check for Header
+		addFeature("Has a proper header", 5, new ProperHeaderTestCase("COMP110-001, Fall 2014"));
+		//Check for Capitalization
+		addFeature("Naming Style",5,new CapitalizationStyle());
+	
+		
+	}
+}
---
+++ src/main/java/gradingTools/comp110f14/assignment6testcases/PromptTest.java
@@ -0,0 +1,5 @@
+package gradingTools.comp110f14.assignment6testcases;
+
+public class PromptTest {//just put this here so package would push. Thanks spencer!
+
+}
