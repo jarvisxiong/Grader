e9c0b4a219ea20a9a03997d9f1ad67b7f67f3eb4
unknown <mdaum@compsci914.cs.unc.edu>
2015-01-30 14:03:39 -0500

    a2 grader packages up

---
+++ src/main/java/gradingTools/comp110s15/assignment2/Assignment2Requirements.java
@@ -0,0 +1,5 @@
+package gradingTools.comp110s15.assignment2;
+
+public class Assignment2Requirements {
+
+}
---
+++ src/main/java/gradingTools/comp110s15/assignment2/testcases/placeholder.java
@@ -0,0 +1,5 @@
+package gradingTools.comp110s15.assignment2.testcases;
+
+public class placeholder {
+
+}
