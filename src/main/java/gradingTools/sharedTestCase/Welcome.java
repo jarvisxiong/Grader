803a212cc2d89c7d2ebd1c17d52b0af859387cd0
unknown <daumisgod@gmail.com>
2015-04-17 00:28:39 -0400

    did grader 6

---
+++ src/main/java/gradingTools/sharedTestCase/Welcome.java
@@ -0,0 +1,29 @@
+package gradingTools.sharedTestCase;
+
+import framework.execution.RunningProject;
+import framework.grading.testing.BasicTestCase;
+import framework.grading.testing.NotAutomatableException;
+import framework.grading.testing.NotGradableException;
+import framework.grading.testing.TestCaseResult;
+import framework.project.Project;
+import gradingTools.utils.RunningProjectUtils;
+
+public class Welcome extends BasicTestCase{
+
+	public Welcome() {
+		super("Does initial message contain welcome");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public TestCaseResult test(Project project, boolean autoGrade)
+			throws NotAutomatableException, NotGradableException {
+		RunningProject Project0 = RunningProjectUtils.runProject(project, 10,
+				"");
+		String output0=Project0.await();
+		output0=output0.toLowerCase();
+		if(output0.contains("welcome")) return pass();
+		else return fail("does not contain the keyword specified");
+	}
+
+}
