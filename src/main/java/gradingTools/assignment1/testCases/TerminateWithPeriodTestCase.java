320c062bf9608b495af0759b5ff55dd49a49d88a
pdewan <dewan@cs.unc.edu>
2015-09-06 13:27:42 -0400

    Terminate with period test case

--- src/main/java/gradingTools/assignment1/testCases/TerminateWithPeriodTestCase.java
+++ src/main/java/gradingTools/assignment1/testCases/TerminateWithPeriodTestCase.java
@@ -21,26 +21,61 @@ public class TerminateWithPeriodTestCase extends BasicTestCase {
         super("Terminates with period test case");
     }
 
+//    @Override
+//    public TestCaseResult test(Project project, boolean autoGrade) throws NotAutomatableException, NotGradableException {
+//        try {
+//            // First check that no period keeps it running
+//            FlexibleProgramRunner runner1 = new FlexibleProgramRunner(project, "19 28", false);
+//            String output = runner1.runWithSpaces();
+//            String output2 = runner1.runNoSpaces();
+//            if (!output.contains("*** TIMEOUT ***") && !output2.contains("*** TIMEOUT ***"))
+//                return fail("Program terminates prematurely", autoGrade);
+//
+//            // Check that a period terminates it
+//            FlexibleProgramRunner runner2 = new FlexibleProgramRunner(project, "19 28");
+//
+//            output = runner2.runWithSpaces();
+//            if (!output.contains("*** TIMEOUT ***"))
+//                return pass(autoGrade);
+//            output = runner2.runNoSpaces();
+//            if (!output.contains("*** TIMEOUT ***"))
+//                return pass(autoGrade);
+//            return fail("Program should terminate with period.", autoGrade);
+//
+//        } catch (NotRunnableException e) {
+//            throw new NotGradableException();
+//        }
+//    }
+    boolean validOutput(String anOutput){
+    	return anOutput.contains("30") && anOutput.contains("50");
+    }
+    boolean hasError(String anError){
+    	return !anError.isEmpty();
+    }
+    TestCaseResult test (Project project, String anInput, boolean autoGrade) {
+    	FlexibleProgramRunner runner = new FlexibleProgramRunner(project, anInput, true);
+        String output = runner.runWithSpaces();
+        if (validOutput(output)) {
+        	String error = runner.getRunningProject().getErrorOutput();
+        	if (hasError(error)) {
+        		return fail("Program terminates with errors", autoGrade);
+        	} else {
+        		return pass(autoGrade);
+        	}
+        }
+        return null;
+    }
     @Override
     public TestCaseResult test(Project project, boolean autoGrade) throws NotAutomatableException, NotGradableException {
         try {
-            // First check that no period keeps it running
-            FlexibleProgramRunner runner1 = new FlexibleProgramRunner(project, "19 28", false);
-            String output = runner1.runWithSpaces();
-            String output2 = runner1.runNoSpaces();
-            if (!output.contains("*** TIMEOUT ***") && !output2.contains("*** TIMEOUT ***"))
-                return fail("Program terminates prematurely", autoGrade);
-
-            // Check that a period terminates it
-            FlexibleProgramRunner runner2 = new FlexibleProgramRunner(project, "19 28");
-
-            output = runner2.runWithSpaces();
-            if (!output.contains("*** TIMEOUT ***"))
-                return pass(autoGrade);
-            output = runner2.runNoSpaces();
-            if (!output.contains("*** TIMEOUT ***"))
-                return pass(autoGrade);
-            return fail("Program should terminate with period.", autoGrade);
+            TestCaseResult retVal = test(project, "10 20 \n20 30 ", autoGrade);
+            if (retVal == null) {
+            	retVal = test(project, "10 20\20 30", autoGrade);
+            }
+            if (retVal == null) {
+            	return fail("Program terminates with errors", autoGrade);
+            }
+            return retVal;
 
         } catch (NotRunnableException e) {
             throw new NotGradableException();
