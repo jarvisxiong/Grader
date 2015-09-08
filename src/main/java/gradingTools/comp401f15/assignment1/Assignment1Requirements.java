1e55a97e7b6057ace643050be544c2ced3c1f4c8
pdewan <dewan@cs.unc.edu>
2015-09-08 18:35:49 -0400

    A3 due date

--- src/main/java/gradingTools/comp401f15/assignment1/Assignment1Requirements.java
+++ src/main/java/gradingTools/comp401f15/assignment1/Assignment1Requirements.java
@@ -23,6 +23,10 @@ import gradingTools.sharedTestCase.MinDeclaredMethodsTestCase;
 public class Assignment1Requirements extends FrameworkProjectRequirements {
 
     public Assignment1Requirements() {
+    	addDueDate("08/26/2015 23:55:00", 1.05);
+    	addDueDate("08/28/2015 23:55:00", 1);
+    	addDueDate("08/30/2015 23:55:00", 0.9);
+    	addDueDate("09/01/2015 23:55:00", 0.75);
     	
         // Functionality
         addFeature("Process & print tokens", 40,
