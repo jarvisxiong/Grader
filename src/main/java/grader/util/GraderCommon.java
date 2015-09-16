640cc110aef057b9d9fd0bfb7531726dba57bff1
pdewan <dewan@cs.unc.edu>
2015-09-15 20:30:33 -0400

    added .jpeg suffix

--- src/main/java/grader/util/GraderCommon.java
+++ src/main/java/grader/util/GraderCommon.java
@@ -2,7 +2,7 @@ package grader.util;
 
 public class GraderCommon {
 	public static String[] docSuffixes = {".doc", ".docx", ".pdf", ".ppt", ".pptx", ".txt", ".png", ".jpg", ".gif", ".tiff", ".bmp"};
-	public static String[] imageSuffixes = {".pdf", ".png", ".jpg", ".gif", ".tiff", ".bmp", ".docx", ".doc"};
+	public static String[] imageSuffixes = {".pdf", ".png", ".jpg", ".jpeg", ".gif", ".tiff", ".bmp", ".docx", ".doc"};
 
 	
 	
