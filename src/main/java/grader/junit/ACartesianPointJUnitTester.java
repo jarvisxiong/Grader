eb2978f6630bbc7bb13c6c39423531668287cbf8
pdewan <dewan@cs.unc.edu>
2016-03-19 11:50:25 -0400

    Junit test stuff from JavaTeaching

---
+++ src/main/java/grader/junit/ACartesianPointJUnitTester.java
@@ -0,0 +1,25 @@
+package grader.junit;
+
+
+//import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ACartesianPointJUnitTester {	
+	@Test
+	public void test() {
+		test(10, 0, 10.0, 0); // 0 degree angle
+		test(0, 10, 10.0, Math.PI / 2); // 90 degree angle
+		test(0, -10, 10.0, -Math.PI / 2); // -90 degree angle
+		test(-10, 0, 10.0, Math.PI); // 180 degree angle
+	}	
+	public void test(int theX, int theY, double theCorrectRadius,
+			double theCorrectAngle) {
+		Point point = new ACartesianPoint(theX, theY);
+		double computedRadius = point.getRadius();
+		double computedAngle = point.getAngle();	
+		Assert.assertTrue(computedRadius == theCorrectRadius);
+		Assert.assertTrue(computedAngle == theCorrectAngle);	
+	}
+
+}
---
+++ src/main/java/grader/junit/ASinglePointBeforeClassJUnitMultiTester.java
@@ -0,0 +1,30 @@
+package grader.junit;
+
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ASinglePointBeforeClassJUnitMultiTester {	
+	static Point point;
+	@BeforeClass
+	public static void createPoint() {
+		System.out.println("Testing 10, 10");
+		point = new ACartesianPoint(10, 10);
+	}
+	
+	@Test
+	public void testAngle() {
+		double computedAngle = point.getAngle();	
+		Assert.assertTrue(computedAngle == Math.PI/4);
+
+	}
+	@Test
+	public void testRadius() {
+		double computedRadius = point.getRadius();
+		Assert.assertTrue(computedRadius == Math.sqrt(200));
+		
+	}
+	
+
+}
