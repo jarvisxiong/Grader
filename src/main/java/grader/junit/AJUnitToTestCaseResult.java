85d8f14ca561d7379ae79ed38c94931f1334d6d1
pdewan <dewan@cs.unc.edu>
2016-05-08 14:15:35 -0400

    Junit exception to test result

---
+++ src/main/java/grader/junit/AJUnitToTestCaseResult.java
@@ -0,0 +1,61 @@
+package grader.junit;
+
+import org.junit.runner.Description;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
+
+import framework.grading.testing.CheckResult;
+import framework.grading.testing.TestCaseResult;
+
+public class AJUnitToTestCaseResult extends RunListener {
+	TestCaseResult checkResult;
+	String name = "";
+	
+	public void setJUnitName(String aName) {
+		name = aName;
+	}
+	
+	public String getJUnitName() {
+		return name;
+	}
+
+	@Override
+	public void testFailure(Failure failure) {
+	       try {
+			super.testFailure(failure);
+			Throwable aThrowable = failure.getException();
+			String aMessage = aThrowable.getMessage();
+			String[] aNotesAndScore = aMessage.split(":");
+			String aNotes = aNotesAndScore[0];
+			if (aNotesAndScore.length == 1) {
+				checkResult = new TestCaseResult(false, 
+						aNotes, name, true);
+			} else {
+				String aPercentageString = aNotesAndScore[1].trim();
+				try {
+				double aPercentage = Double.parseDouble(aPercentageString);
+				checkResult =new TestCaseResult(aPercentage, aNotes, name, true);
+
+				} catch (Exception e) {
+					
+				}
+				
+			}
+//			System.out.println ("Failure:" + failure);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void testFinished(Description description) {
+		try {
+			super.testFinished(description);
+//			System.out.println ("Test finished:"+ description);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		};
+		
+	}
+
+}
