ebc9387ea5c4966e959d320d2753524498d4de54
pdewan <dewan@cs.unc.edu>
2016-06-19 00:40:02 -0400

    File not included

---
+++ src/main/java/grader/execution/FlexibleMainClassFinder.java
@@ -0,0 +1,15 @@
+package grader.execution;
+
+import framework.execution.NotRunnableException;
+import framework.project.BasicProject;
+import grader.project.Project;
+import grader.project.folder.RootCodeFolder;
+
+import java.util.Map;
+
+public interface FlexibleMainClassFinder extends MainClassFinder{
+	public Class mainClass(RootCodeFolder aRootCodeFolder, ProxyClassLoader aProxyClassLoader, String expectedName, Project aProject);
+//    public Map<String, String> getEntryPoints(framework.project.Project project, String aSpecifiedMainClass) throws NotRunnableException;
+
+
+}
