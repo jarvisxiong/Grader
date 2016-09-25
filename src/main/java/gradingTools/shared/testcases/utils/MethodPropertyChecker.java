943d84b41d4b1f89c04b00bfa3dc05987161fecc
pdewan <dewan@cs.unc.edu>
2016-09-25 11:27:51 -0400

    Removed shared.test.cases to remove duplicate classes

--- src/main/java/gradingTools/shared/testcases/utils/MethodPropertyChecker.java
+++
@@ -1,8 +0,0 @@
-package gradingTools.shared.testcases.utils;
-
-import java.lang.reflect.Method;
-
-public interface MethodPropertyChecker {
-
-    public String getMessageOnIncorrect(Method method);
-}
