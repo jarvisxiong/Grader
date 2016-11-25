0da6b07d2ed3126057bf635d176d2d2efe3d7fb3
Semion Piskarev <semionpiskarev@gmail.com>
2016-11-25 16:16:00 -0500

    Edited typeRegex to work with tags and qualified class names

--- src/main/java/gradingTools/sharedTestCase/checkstyle/CheckStyleTestCase.java
+++ src/main/java/gradingTools/sharedTestCase/checkstyle/CheckStyleTestCase.java
@@ -42,7 +42,7 @@ public abstract class CheckStyleTestCase extends BasicTestCase {
 //	}
 	
 	protected String typeRegex(String aTypeTag) {
-        return "(.*)" + "Class" + "(.*)" + "matching" + "(.*)@" + aTypeTag + "[ /](.*)" + "defined" + "(.*)" ;
+        return "(.*)" + "Class" + "(.*)" + "matching" + "(.*)[@ ]" + aTypeTag + "[ /](.*)" + "defined" + "(.*)" ;
     }
     
     protected boolean failOnMatch() {
